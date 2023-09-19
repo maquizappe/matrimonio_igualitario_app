af8c7301fdfa15fbb65e40cfe864a59d020ba9bc
maquizappe <macarena.zappe@gmail.com>
2023-09-19 12:26:57 -0300

    .

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+/* module.exports = {
     basePath: process.env.NODE_ENV === 'production' ? '/matrimonio_igualitario_app' : '',
     assetPrefix: process.env.NODE_ENV === 'production' ? '/matrimonio_igualitario_app/' : '',
     trailingSlash: true, // Ensure trailing slashes are used for routes
@@ -10,5 +10,11 @@ module.exports = {
       // Specify that you want to export the site as static HTML
       export: true,
     },
-  };
+  }; */
+
+const nextConfig = {}
+
+ 
+
+module.exports = nextConfig
   
