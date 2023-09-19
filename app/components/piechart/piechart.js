51dcd85acb6c95bd04ee8852749366ffd22eefca
maquizappe <macarena.zappe@gmail.com>
2023-09-19 15:11:16 -0300

    piechart

---
+++ app/components/piechart/piechart.js
@@ -0,0 +1,15 @@
+function Piechart() {
+      
+    return (
+      <div className="App">
+           <div className="pie">
+      <img src="./pie.png" alt="pie" />
+      </div>
+      <style>
+      
+      </style>
+    </div>
+   
+    );
+  }
+  export default Piechart;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,21 @@
 
 import React from "react";
 import Cover from "./components/cover/cover"
-
+import Piechart from "./components/piechart/piechart"
+import './globals.css'
 export default function Home() {
+
   return (
     <>   
+    
+    <div className="contentwhole"> 
     <div> <Cover> </Cover></div>
+
+    <div> <Piechart> </Piechart></div>
+    </div>
+
+
+
     </>
   )
 }
