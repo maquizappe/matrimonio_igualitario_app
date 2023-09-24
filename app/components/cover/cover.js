115316ce0f2edb681acef5f750771fed06816243
maquizappe <macarena.zappe@gmail.com>
2023-09-24 11:21:21 -0300

    cursor

--- app/components/cover/cover.js
+++ app/components/cover/cover.js
@@ -1,12 +1,12 @@
 import './cover.css'
 import React, { useRef, useState } from "react";
-/* import useCursorHandlers from '@/app/components/cursor/useCursorHandlers'; */
+import useCursorHandlers from '@/app/components/cursor/useCursorHandlers'; 
 
 function Cover() {
-/*   const cursorHandlers = useCursorHandlers(); */
+ const cursorHandlers = useCursorHandlers(); 
 
   return (
-    <div className="App" /*  {...cursorHandlers} */>  
+    <div className="App"  {...cursorHandlers} >  
       <div className="contentcover">
         <div className="contentTile">
           <div className="title">Matrimonio</div>
